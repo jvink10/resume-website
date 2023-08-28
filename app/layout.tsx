2d8964961e93fc1746c200f32cbd79938b7c90bb
jvink10 <jordanvink10@gmail.com>
2023-08-28 23:14:57 +1000

    Added ; to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,14 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
 	title: 'Jordan Vink',
   	description: 'Resume website of Jordan Vink',
-}
+};
 
 export default function RootLayout({
   	children,
@@ -17,20 +17,20 @@ export default function RootLayout({
 }) {
   	return (
     	<html lang="en">
-      		<body className={`${inter.className} flex flex-col min-h-screen`}>
+      		<body className={`${inter.className} flex flex-col min-h-screen text-white`}>
 				<header className="py-4 bg-zinc-800">
 					<div className="container mx-auto px-4">
-						<h1 className="text-xl text-white font-bold">Jordan Vink</h1>
+						<h1 className="text-xl font-bold">Jordan Vink</h1>
 					</div>
 				</header>
       			{children}
 				<Analytics />
-				<footer className="py-4 bg-zinc-100">
+				<footer className="py-4 bg-zinc-600">
 					<div className="container mx-auto px-4">
-						<p className="text-center text-black">Footer</p>
+						<p className="text-center">Jordan Vink</p>
 					</div>
 				</footer>
       		</body>
     	</html>
-  	)
-}
+  	);
+};
