42841563b44c82b7c0a135ff0460b7af69b0d4b7
jvink10 <jordanvink10@gmail.com>
2023-08-29 16:54:04 +1000

    Minor alterations
    Changed the order of the prior work cards and made links open in new tabs.

--- app/page.tsx
+++ app/page.tsx
@@ -18,10 +18,10 @@ export default function Home() {
 					</div>
 				</div>
 				<div className="mb-8">
-					<ProjectSummary title="Wamos Portugal Web Development Intern" image="/wamos-grupo-logo.png" imageWidth={238} imageHeight={77} summary="Four month internship at Wamos Portugal, tasked with completing an internal security test and the rework of a data table interface." />
+					<ProjectSummary title="Myxos and Musings Book Sale" image="/open-page-95.jpg" imageWidth={200} imageHeight={133} summary="Managed pricing and design for the sale of the Myxos and Musings book." />
 				</div>
 				<div>
-					<ProjectSummary title="Myxos and Musings Book Sale" image="/open-page-95.jpg" imageWidth={200} imageHeight={133} summary="Managed pricing and design for the sale of the Myxos and Musings book." />
+					<ProjectSummary title="Wamos Portugal Web Development Intern" image="/wamos-grupo-logo.png" imageWidth={238} imageHeight={77} summary="Four month internship at Wamos Portugal, tasked with completing an internal security test and the rework of a data table interface." />
 				</div>
 			</section>
 		</main>
--- components/LinkedinIcon.tsx
+++ components/LinkedinIcon.tsx
@@ -3,7 +3,7 @@ import { FaLinkedin } from 'react-icons/fa';
 
 export default function LinkedinIcon() {
     return (
-        <Link href="https://www.linkedin.com/in/jordan-vink">
+        <Link href="https://www.linkedin.com/in/jordan-vink" target="_blank">
             <FaLinkedin size={28} />
         </Link>
     );
