9e7b3e0dfb63909dc721a0a87be172f6fffc3748
jvink10 <jordanvink10@gmail.com>
2023-09-01 17:12:11 +1000

    Update mobile layout

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -6,38 +6,38 @@ export default function Work() {
     return (
         <main className="flex-grow bg-slate-700">
             <section className="my-16 space-y-16">
-                <div id="myxos-and-musings" className="flex flex-row">
-                    <div className="basis-1/2 space-y-4">
-                        <h2 className="ml-64 text-xl">Myxos and Musings Book Sale</h2>
-                        <div className="ml-64 space-x-2">
+                <div id="myxos-and-musings" className="flex flex-col lg:flex-row">
+                    <div className="basis-1/2 mx-8 sm:mx-16 lg:ml-32 xl:ml-48 2xl:ml-64 ">
+                        <h2 className="mb-2 text-xl">Myxos and Musings Book Sale</h2>
+                        <div className="mb-4 space-x-2 space-y-2">
                             <SkillTag text="Next.js" />
                             <SkillTag text="Tailwind CSS" />
                             <SkillTag text="Typescript" />
                             <SkillTag text="React" />
                         </div>
-                        <div className="ml-64">
+                        <div>
                             <p>Designed and developed the front end, back end, and payment system of the website for the sale of the Myxos and Musings book by Steve Young.</p>
                         </div>
                     </div>
-                    <div className="basis-1/2">
+                    <div className="basis-1/2 mt-8 lg:mt-0">
                         <Image src="/work/myxos-and-musings/myxos-and-musings-collage.png" alt="Collage of the Myxos and Musings website" width={960} height={540} />
                     </div>
                 </div>
-                <div id="wamos-portugal" className="flex flex-row-reverse">
-                    <div className="basis-1/2 space-y-4 text-right">
-                        <h2 className="mr-64 text-xl">Wamos Portugal Web Development Intern</h2>
-                        <div className="mr-64 space-x-2">
+                <div id="wamos-portugal" className="flex flex-col lg:flex-row-reverse">
+                    <div className="basis-1/2 mx-8 sm:mx-16 lg:mr-32 xl:mr-48 2xl:mr-64 text-right">
+                        <h2 className="mb-2 text-xl">Wamos Portugal Web Development Intern</h2>
+                        <div className="mb-4 space-x-2 space-y-2">
                             <SkillTag text="Laravel" />
                             <SkillTag text="SASS" />
                             <SkillTag text="jQuery" />
                             <SkillTag text="PHP" />
                         </div>
-                        <div className="mr-64">
+                        <div>
                             <p>Four month internship at Wamos Portugal, tasked with completing an internal security test and the rework of a data table interface.</p>
                         </div>
                     </div>
                     <div className="basis-1/2">
-                        <Image src="/work/wamos-portugal/wamos-air-plane.png" alt="Wamos air plane" width={1280} height={853} className="ml-32" />
+                        <Image src="/work/wamos-portugal/wamos-air-plane.png" alt="Wamos air plane" width={1280} height={853} className="lg:ml-16 xl:ml-24 2xl:ml-32" />
                     </div>
                 </div>
             </section>
