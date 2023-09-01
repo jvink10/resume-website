7ca5f65f8f3f373662fad41b367248cf24bbd4bd
jvink10 <jordanvink10@gmail.com>
2023-09-01 14:27:31 +1000

    Update work page format

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -7,32 +7,36 @@ export default function Work() {
         <main className="flex-grow bg-slate-700">
             <section className="mt-16 space-y-16">
                 <div id="myxos-and-musings" className="flex flex-row">
-                    <div className="ml-64">
-                        <h2 className="mb-4 text-xl">Myxos and Musings Book Sale</h2>
-                        <div className="space-x-2">
+                    <div className="basis-1/2 space-y-4">
+                        <h2 className="ml-64 text-xl">Myxos and Musings Book Sale</h2>
+                        <div className="ml-64 space-x-2">
                             <SkillTag text="Next.js" />
                             <SkillTag text="Tailwind CSS" />
                             <SkillTag text="Typescript" />
                             <SkillTag text="React" />
                         </div>
-                        <p></p>
+                        <div className="ml-64">
+                            <p>Designed and developed the front end, back end and payment system of the website for the sale of the Myxos and Musings book by Steve Young.</p>
+                        </div>
                     </div>
-                    <div>
+                    <div className="basis-1/2">
                         {/*Image of the website here*/}
                     </div>
                 </div>
                 <div id="wamos-portugal" className="flex flex-row-reverse">
-                    <div className="mr-64">
-                        <h2 className="mb-4 text-right text-xl">Wamos Portugal Web Development Intern</h2>
-                        <div className="space-x-2 text-right">
+                    <div className="basis-1/2 space-y-4">
+                        <h2 className="mr-64 text-right text-xl">Wamos Portugal Web Development Intern</h2>
+                        <div className="mr-64 space-x-2 text-right">
                             <SkillTag text="Laravel" />
                             <SkillTag text="SASS" />
                             <SkillTag text="jQuery" />
                             <SkillTag text="PHP" />
                         </div>
-                        <p></p>
+                        <div className="mr-64">
+                            <p>Four month internship at Wamos Portugal, tasked with completing an internal security test and the rework of a data table interface.</p>
+                        </div>
                     </div>
-                    <div>
+                    <div className="basis-1/2">
                         {/*Some imagery*/}
                     </div>
                 </div>
