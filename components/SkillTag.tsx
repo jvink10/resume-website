8ff7500a8f6d89c09f39590be26624d141c5b462
jvink10 <jordanvink10@gmail.com>
2023-09-01 14:04:11 +1000

    Create SkillTag component

---
+++ components/SkillTag.tsx
@@ -0,0 +1,18 @@
+type Props = {
+    text: string;
+    textColour?: string;
+    bgColour?: string;
+};
+
+export default function SkillTag(props: Props) {
+    const {
+        textColour = "black",
+        bgColour = "white",
+    } = props;
+
+    return (
+        <div className={`inline-block rounded-full bg-${bgColour}`}>
+            <p className={`px-4 py-1 text-${textColour}`}>{props.text}</p>
+        </div>
+    );
+};
