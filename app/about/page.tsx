85d175181ffa644a1e49973065c9b83be9db35ad
jvink10 <jordanvink10@gmail.com>
2023-09-02 12:45:28 +1000

    Update about page layout

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,11 +3,11 @@ import PhotoSphere from '../../components/PhotoSphere';
 export default function About() {
     return (
         <main className="flex-grow bg-slate-700">
-            <div className="flex flex-col lg:flex-row mx-64 my-16 space-x-16">
-                <div>
+            <div className="flex flex-col mx-auto my-16">
+                <div className="mx-auto max-w-[516px] text-center">
                     <p>Jordan enjoys taking photo spheres with his phone, and going on long walks, which he combines into some fantastic photo sphere shots of his home city, Brisbane</p>
                 </div>
-                <div className="inline-block p-2 bg-white">
+                <div className="inline-block mx-auto p-2 max-w-max bg-white">
                     <PhotoSphere image="/photo-spheres/bardon-esplanade-park.jpg" imageWidth={512} imageHeight={384} />
                 </div>
             </div>
