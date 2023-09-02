cf20833da1ee763f5ace0f3c3ebca9f4e5c75ae0
jvink10 <jordanvink10@gmail.com>
2023-09-03 09:29:11 +1000

    Add gradient border

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -5,13 +5,15 @@ export default function Contact() {
     return (
         <main className="flex-grow bg-slate-700">
             <section className="py-16 space-y-8 text-center">
-                <h1 className="text-2xl">Want To Get In Contact?</h1>
+                <div className="mx-8 md:mx-16 lg:mx-32 bg-gradient-to-r from-transparent via-white to-transparent py-px">
+					<div className="py-4 w-full h-full bg-slate-700">
+                    <h1 className="text-center text-xl">Want To Get In Contact?</h1>
+                    </div>
+				</div>
                 <div className="space-y-4">
                     <Link href="https://www.linkedin.com/in/jordan-vink" target="_blank" className="block underline">Visit My Linkedin</Link>
                     <Link href="https://github.com/jvink10" target="_blank" className="block underline">Check Out My Github</Link>
-                </div>
-                <div className="hidden">
-                    <ContactForm />
+                    <p>Contact me at jordanvink10@gmail.com</p>
                 </div>
             </section>
         </main>
