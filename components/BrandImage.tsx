379b4233155842aaed60df206185631ab40621d3
jvink10 <jordanvink10@gmail.com>
2023-09-03 10:44:19 +1000

    Add brand logo no slogan

--- components/BrandImage.tsx
+++ components/BrandImage.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function BrandImage() {
     return (
         <div>
-            <Image src="/brand/logo-transparent.png" alt="Vink web development logo" width={200} height={76} />
+            <Image src="/brand/logo-transparent-no-slogan.png" alt="Vink web development logo" width={200} height={56} />
         </div>
     );
 };
