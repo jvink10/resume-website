5139ca350601438a171c5e183fefd97ae8e155a6
jvink10 <jordanvink10@gmail.com>
2023-09-04 13:23:53 +1000

    Add myxos and musings css animation

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -6,8 +6,8 @@ import SkillTag from '../../components/SkillTag';
 export default function Work() {
     return (
         <main className="flex-grow bg-slate-700">
-            <section className="my-16 space-y-16">
-                <div id="myxos-and-musings" className="flex flex-col lg:flex-row justify-evenly mx-8 space-x-4">
+            <section>
+                <div id="myxos-and-musings" className="flex flex-col lg:flex-row justify-evenly mx-8 my-16 gap-x-4">
                     <div className="basis-1/2 self-center mx-4 sm:mx-8 lg:mx-0 lg:max-w-[512px]">
                         <h2 className="mb-2 text-xl">Myxos and Musings Book Sale</h2>
                         <div className="mb-4">
@@ -20,12 +20,12 @@ export default function Work() {
                             <p>Designed and developed the front end, back end, and payment system of the website for the sale of the Myxos and Musings book by Steve Young.</p>
                         </div>
                     </div>
-                    <div className="basis-1/2 mx-auto lg:mx-0 mt-8 lg:mt-0">
-                        <Image src="/work/myxos-and-musings/myxos-and-musings-collage.png" alt="Collage of the Myxos and Musings website" width={960} height={540} className="hidden lg:block transition hover:scale-105" />
-                        <Image src="/work/myxos-and-musings/myxos-and-musings-home-desktop.png" alt="Home page of the Myxos and Musings website" width={901} height={1444} className="block lg:hidden" />
+                    <div className="relative group basis-1/2 mx-auto lg:mx-0 mt-8 lg:mt-0 lg:h-96 xl:h-[512px] 2xl:h-[640px]">
+                        <Image src="/work/myxos-and-musings/myxos-and-musings-home-desktop.png" alt="Home page of the Myxos and Musings website" width={450} height={722} className="relative lg:absolute inset-y-0 lg:left-0 w-full h-full object-contain transition group-hover:scale-105 z-10" />
+                        <Image src="/work/myxos-and-musings/myxos-and-musings-purchase-desktop-90.png" alt="Purchase page of the Myxos and Musings website" width={505} height={908} className="absolute inset-y-0 lg:-left-16 lg:top-8 w-full h-1/2 lg:h-full object-contain transition group-hover:rotate-90 lg:group-hover:translate-x-16 lg:group-hover:-translate-y-8 group-hover:scale-[1.4] lg:group-hover:scale-105 group-hover:z-20" />
                     </div>
                 </div>
-                <div id="wamos-portugal" className="flex flex-col lg:flex-row-reverse justify-evenly mx-8 space-x-4">
+                <div id="wamos-portugal" className="flex flex-col lg:flex-row-reverse justify-evenly mx-8 my-16 gap-x-4">
                     <div className="basis-1/2 mx-4 sm:mx-8 lg:mx-0 lg:max-w-[512px] lg:text-right">
                         <h2 className="mb-2 text-xl">Wamos Portugal Web Development Intern</h2>
                         <div className="mb-4">
