5852b7e1bb5a5deacbaeb421e142598cfc76a124
jvink10 <jordanvink10@gmail.com>
2023-09-05 15:50:15 +1000

    Add focus pseudo class to nav

--- components/nav/MobileNavBar.tsx
+++ components/nav/MobileNavBar.tsx
@@ -23,7 +23,7 @@ export default function MobileNavBar() {
             </div>
             <div>
                 {isOpen && (
-                    <div className="w-full h-full fixed top-0 right-0 z-10">
+                    <div className="w-full h-full fixed top-0 right-0 z-50">
                         <button onClick={toggleMenu} className="absolute w-1/2 h-full top-0 right-0 bg-black opacity-50"></button>
                         <div className="absolute w-1/2 h-full top-0 left-0 bg-slate-700">
                             <div className="mb-4 pb-px bg-gradient-to-r from-transparent via-white to-transparent">
@@ -35,16 +35,16 @@ export default function MobileNavBar() {
                             </div>
                             <ul className="text-lg text-center">
                                 <li>
-                                    <Link href="/" onClick={toggleMenu} className={`block py-4 ${pathname === "/" ? "text-brand-blue hover:text-brand-blue-500" : "hover:text-brand-blue-200"}`}>Home</Link>
+                                    <Link href="/" onClick={toggleMenu} className={`block py-4 ${pathname === "/" ? "text-brand-blue hover:text-brand-blue-500 active:text-brand-blue-700" : "hover:text-brand-blue-200 active:text-brand-blue-500"}`}>Home</Link>
                                 </li>
                                 <li>
-                                    <Link href="/work" onClick={toggleMenu} className={`block py-4 ${pathname === "/work" ? "text-brand-blue hover:text-brand-blue-500" : "hover:text-brand-blue-200"}`}>Work</Link>
+                                    <Link href="/work" onClick={toggleMenu} className={`block py-4 ${pathname === "/work" ? "text-brand-blue hover:text-brand-blue-500 active:text-brand-blue-700" : "hover:text-brand-blue-200 active:text-brand-blue-500"}`}>Work</Link>
                                 </li>
                                 <li>
-                                    <Link href="/about" onClick={toggleMenu} className={`block py-4 ${pathname === "/about" ? "text-brand-blue hover:text-brand-blue-500" : "hover:text-brand-blue-200"}`}>About</Link>
+                                    <Link href="/about" onClick={toggleMenu} className={`block py-4 ${pathname === "/about" ? "text-brand-blue hover:text-brand-blue-500 active:text-brand-blue-700" : "hover:text-brand-blue-200 active:text-brand-blue-500"}`}>About</Link>
                                 </li>
                                 <li>
-                                    <Link href="/contact" onClick={toggleMenu} className={`block py-4 ${pathname === "/contact" ? "text-brand-blue hover:text-brand-blue-500" : "hover:text-brand-blue-200"}`}>Contact</Link>
+                                    <Link href="/contact" onClick={toggleMenu} className={`block py-4 ${pathname === "/contact" ? "text-brand-blue hover:text-brand-blue-500 active:text-brand-blue-700" : "hover:text-brand-blue-200 active:text-brand-blue-500"}`}>Contact</Link>
                                 </li>
                             </ul>
                             <div className="mx-2 mt-4 text-center">
--- components/nav/NavBar.tsx
+++ components/nav/NavBar.tsx
@@ -7,10 +7,10 @@ export default function Navbar() {
 
     return (
         <div>
-            <Link href="/" className={`px-4 py-6 ${pathname === "/" ? "text-brand-blue hover:text-brand-blue-500" : "hover:text-brand-blue-200"}`}>Home</Link>
-			<Link href="/work" className={`px-4 py-6 ${pathname === "/work" ? "text-brand-blue hover:text-brand-blue-500" : "hover:text-brand-blue-200"}`}>Work</Link>
-			<Link href="/about" className={`px-4 py-6 ${pathname === "/about" ? "text-brand-blue hover:text-brand-blue-500" : "hover:text-brand-blue-200"}`}>About</Link>
-			<Link href="/contact" className={`px-4 py-6 ${pathname === "/contact" ? "text-brand-blue hover:text-brand-blue-500" : "hover:text-brand-blue-200"}`}>Contact</Link>
+            <Link href="/" className={`px-4 py-6 ${pathname === "/" ? "text-brand-blue hover:text-brand-blue-500 active:text-brand-blue-700" : "hover:text-brand-blue-200 active:text-brand-blue-500"}`}>Home</Link>
+			<Link href="/work" className={`px-4 py-6 ${pathname === "/work" ? "text-brand-blue hover:text-brand-blue-500 active:text-brand-blue-700" : "hover:text-brand-blue-200 active:text-brand-blue-500"}`}>Work</Link>
+			<Link href="/about" className={`px-4 py-6 ${pathname === "/about" ? "text-brand-blue hover:text-brand-blue-500 active:text-brand-blue-700" : "hover:text-brand-blue-200 active:text-brand-blue-500"}`}>About</Link>
+			<Link href="/contact" className={`px-4 py-6 ${pathname === "/contact" ? "text-brand-blue hover:text-brand-blue-500 active:text-brand-blue-700" : "hover:text-brand-blue-200 active:text-brand-blue-500"}`}>Contact</Link>
         </div>
     );
 };
